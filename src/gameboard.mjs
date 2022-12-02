f2f92d213c8a89daa3062c6d19799e59a054082f
TaftSound <mattmurphytattoos@gmail.com>
2022-12-01 18:32:42 -0800

    Implement computer opponent intelligent ship placement

--- src/gameboard.mjs
+++ src/gameboard.mjs
@@ -1,18 +1,79 @@
 import { shipFactory } from './ship-factory.mjs'
 
-// place ships at specific coordiantes - done
+export const getAvailablePlacements = function (row, length, direction, gameboard) {
+  const currentRow = gameboard[row]
+  const availableInRow = []
+  for (const space in currentRow) {
+    if (!currentRow[space]) { continue }
+    let available = false
+    let count = 1
+    for (let i = 1; i < length; i++) {
+      if (direction === 'horizontal') {
+        const index = +space + i
+        if (currentRow[index]) { count++ }
+      } else {
+        const rowIndex = row + i
+        const nextRow = gameboard[rowIndex]
+        if (nextRow) {
+          if (nextRow[space]) { count++ }
+        }
+      }
+    }
+    if (count === length) { available = true }
+    if (available) { availableInRow.push(+space) }
+  }
+  return availableInRow
+}
 
-// recieveAttack function, takes pair of coordinates
-// and determines if there is a hit, the sends hit
-// function to correct ship - done
+export const markUnavailableSpaces = function ([row, column], length, direction, gameboard) {
+  const indexesToChange = []
+  if (direction === 'horizontal') {
+    let firstIndex = column - 1
+    const lastIndex = column + length
+    for (firstIndex; firstIndex <= lastIndex; firstIndex++) {
+      indexesToChange.push([(row - 1), firstIndex])
+      indexesToChange.push([(row), firstIndex])
+      indexesToChange.push([(row + 1), firstIndex])
+    }
+  } else {
+    let firstIndex = row - 1
+    const lastIndex = row + length
+    for (firstIndex; firstIndex <= lastIndex; firstIndex++) {
+      indexesToChange.push([firstIndex, (column - 1)])
+      indexesToChange.push([firstIndex, (column)])
+      indexesToChange.push([firstIndex, (column + 1)])
+    }
+  }
+  indexesToChange.forEach((index) => {
+    if (isIndexOnBoard(index)) {
+      gameboard[index[0]].splice(index[1], 1, 0)
+    }
+  })
+  return gameboard
 
-// records all attacks and whether hits or not - done
-
-// report whether all ships are sunk or not - done
+  function isIndexOnBoard (index) {
+    if (index[0] < 0 || index[0] > 9) {
+      return false
+    } else if (index[1] < 0 || index[1] > 9) {
+      return false
+    } else return true
+  }
+}
 
 export function gameboardFactory () {
   const allShips = []
   const attacksReceived = []
+  let availableSpaces = (() => {
+    const spacesArray = []
+    for (let i = 0; i < 10; i++) {
+      const rowArray = []
+      for (let j = 0; j < 10; j++) {
+        rowArray.push(1)
+      }
+      spacesArray.push(rowArray)
+    }
+    return spacesArray
+  })()
 
   function checkForHit (coordinates) {
     for (const ship in allShips) {
@@ -42,7 +103,28 @@ export function gameboardFactory () {
     placeShip: function (index, length, direction) {
       const ship = shipFactory(length)
       const shipPlacementObject = { ship, index, direction }
+      availableSpaces = markUnavailableSpaces(index, length, direction, availableSpaces)
       allShips.push(shipPlacementObject)
+    },
+    randomizeShipPlacement: function () {
+      const shipLengths = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+      for (const length in shipLengths) {
+        let randomRow = Math.round(Math.random() * 9)
+        let direction = 'horizontal'
+        if (Math.round(Math.random())) { direction = 'vertical' }
+        let availablePlacements = getAvailablePlacements(randomRow, shipLengths[length], direction, availableSpaces)
+        if (!availablePlacements.length) {
+          while (!availablePlacements.length) {
+            randomRow++
+            if (randomRow > 9) { randomRow = 0 }
+            availablePlacements = getAvailablePlacements(randomRow, shipLengths[length], direction, availableSpaces)
+          }
+        }
+        const randomIndex = Math.round(Math.random() * (availablePlacements.length - 1))
+        const randomColumn = availablePlacements[randomIndex]
+        this.placeShip([randomRow, randomColumn], shipLengths[length], direction)
+      }
+      return allShips
     },
     receiveAttack: function (coordinates) {
       const hit = checkForHit(coordinates)
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,4 +1,4 @@
-import { gameboardFactory } from "./gameboard.mjs";
+import { gameboardFactory, getAvailablePlacements, markUnavailableSpaces } from "./gameboard.mjs";
 
 test('placeShip function adds ship', () => {
   const gameboardObject = gameboardFactory()
@@ -73,3 +73,172 @@ test('check that all ships have not been sunk, with multiple ships', () => {
   gameboardObject.receiveAttack([7, 2])
   expect(gameboardObject.areShipsSunk()).toBe(false)
 })
+test('randomize ship creates a ship', () => {
+  const gameboardObject = gameboardFactory()
+  let ships = gameboardObject.randomizeShipPlacement()
+  let firstShip = ships[0]
+  expect(firstShip).toHaveProperty('ship')
+})
+test('first randomized ship has length of 4', () => {
+  const gameboardObject = gameboardFactory()
+  let ships = gameboardObject.randomizeShipPlacement()
+  let firstShip = ships[0]
+  expect(firstShip.ship).toHaveProperty('length', 4)
+})
+test('second randomized ship has length of 3', () => {
+  const gameboardObject = gameboardFactory()
+  let ships = gameboardObject.randomizeShipPlacement()
+  let firstShip = ships[1]
+  expect(firstShip.ship).toHaveProperty('length', 3)
+})
+test('contains all ships', () => {
+  const gameboardObject = gameboardFactory()
+  let ships = gameboardObject.randomizeShipPlacement()
+  expect(ships[0].ship).toHaveProperty('length', 4)
+  expect(ships[1].ship).toHaveProperty('length', 3)
+  expect(ships[2].ship).toHaveProperty('length', 3)
+  expect(ships[3].ship).toHaveProperty('length', 2)
+  expect(ships[4].ship).toHaveProperty('length', 2)
+  expect(ships[5].ship).toHaveProperty('length', 2)
+  expect(ships[6].ship).toHaveProperty('length', 1)
+  expect(ships[7].ship).toHaveProperty('length', 1)
+  expect(ships[8].ship).toHaveProperty('length', 1)
+  expect(ships[9].ship).toHaveProperty('length', 1)
+})
+
+describe('getting available ship placements', () => {
+    const gameboard = [
+      [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+      [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+      [1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+      [1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ] 
+  
+  test('can determine available horizontal moves in a row', () => {
+    expect(getAvailablePlacements(0, 3, 'horizontal', gameboard)).toEqual([4, 5])
+    expect(getAvailablePlacements(1, 3, 'horizontal', gameboard)).toEqual([3, 4])
+    expect(getAvailablePlacements(2, 3, 'horizontal', gameboard)).toEqual([0, 1, 2, 3, 4, 5])
+    expect(getAvailablePlacements(3, 3, 'horizontal', gameboard)).toEqual([0, 1, 2, 3, 4, 5, 6, 7])
+    expect(getAvailablePlacements(0, 2, 'horizontal', gameboard)).toEqual([4, 5, 6])
+    expect(getAvailablePlacements(1, 2, 'horizontal', gameboard)).toEqual([3, 4, 5])
+    expect(getAvailablePlacements(6, 2, 'horizontal', gameboard)).toEqual([0, 6, 7, 8])
+    expect(getAvailablePlacements(6, 3, 'horizontal', gameboard)).toEqual([6, 7])
+  })
+  test('can determine available vertical moves in a row', () => {
+    expect(getAvailablePlacements(0, 3, 'vertical', gameboard)).toEqual([4, 5, 6])
+    expect(getAvailablePlacements(1, 3, 'vertical', gameboard)).toEqual([3, 4, 5, 6])
+    expect(getAvailablePlacements(2, 3, 'vertical', gameboard)).toEqual([0, 1, 2, 3, 4, 5, 6, 7])
+    expect(getAvailablePlacements(3, 3, 'vertical', gameboard)).toEqual([0, 1, 2, 5, 6, 7, 8, 9])
+    expect(getAvailablePlacements(6, 3, 'vertical', gameboard)).toEqual([0, 1, 6, 7, 8, 9])
+    expect(getAvailablePlacements(8, 3, 'vertical', gameboard)).toEqual([])
+    expect(getAvailablePlacements(9, 3, 'vertical', gameboard)).toEqual([])
+  })
+})
+
+describe('marking unavailable spaces', () => {
+  let gameboard = []
+  beforeEach(() => {
+    gameboard = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ] 
+  })
+
+  test('can mark spaces unavailable around horizontal ship', () => {
+    expect(markUnavailableSpaces([3, 4], 3, 'horizontal', gameboard)).toEqual([
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+      [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+      [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ])
+  })
+  test('can mark spaces unavailable around vertical ship', () => {
+    expect(markUnavailableSpaces([2, 6], 3, 'vertical', gameboard)).toEqual([
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ])
+  })
+  test('can mark spaces unavailable around vertical ship', () => {
+    expect(markUnavailableSpaces([0, 6], 3, 'vertical', gameboard)).toEqual([
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ])
+  })
+  test('can mark spaces unavailable around vertical ship', () => {
+    expect(markUnavailableSpaces([4, 9], 3, 'vertical', gameboard)).toEqual([
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ])
+  })
+})
+  test('randomize ship placement', () => {
+    const gameboardObject = gameboardFactory()
+    let filledGameboard = gameboardObject.randomizeShipPlacement()
+    console.log(filledGameboard[0])
+    console.log(filledGameboard[1])
+    console.log(filledGameboard[2])
+    console.log(filledGameboard[3])
+    console.log(filledGameboard[4])
+    console.log(filledGameboard[5])
+    console.log(filledGameboard[6])
+    console.log(filledGameboard[7])
+    console.log(filledGameboard[8])
+    console.log(filledGameboard[9])
+    // [   0  1  2  3  4  5  6  7  8  9
+
+    // 0  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 1  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 2  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 3  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 4  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 5  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 6  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 7  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 8  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    // 9  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    // ]
+  })
